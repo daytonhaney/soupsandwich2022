67ae7bb1ae81516a39317c2f0233cf860bf89cec
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-05-24 23:09:27 -0400

    Update type.py

--- code/type.py
+++ code/type.py
@@ -1,4 +1,6 @@
+#A few examples of type in python3 
 
+------------------------------------------
 def main():
     print(type('Hey, how you doin?'))
     print(type(15))
@@ -7,5 +9,11 @@ def main():
     print(main)
 main()
 
+---------------------------------------
 
-
+# Prints the type of object for example 15 will be 
+#
+#<class 'int'> 
+#at execution 
+# and finally, print(main) will show the programs in memory referance at execution 
+#<function at main 0x10ce57eb0>
