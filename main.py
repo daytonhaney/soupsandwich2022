71e4f3d668ec589de5bb1fc2d485bf84c3170493
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-06-07 01:47:03 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+a = 50
+print(a)
+
+
+def main():
+    print("I doubt this program will ever work")
+
+main()
+
+my_subjects = ['rats','mice','dogs']
+
+print(input("Do u have any lab expirience?"))
+
+
+
+print(input, "Yeah, a little")
---
+++ pydiscussion_week3/main.py
@@ -0,0 +1,27 @@
+"""Jason Preneveau
+    CMIS 120
+    5 June 2022
+"""
+print('\tWelcome to y=mx+b slope calculator\n \tEnter the values as Integers.')
+#using print statements to separate code for visibility and error handling 
+print('----------------------------------------------------------------------')
+
+x=int(input('Enter an integer for x: ')) #promping the user for 3 integers 
+
+
+b =int(input('Enter the y-intercept "b", another integer: ')) #
+m =float(input('Enter the "m" rise over run: '))
+y = m * x + b
+def func( y = m*x+b): # the original slope formula from middle school
+    return y and print(f"'You're Inputs: x={x}, m={m}, b={b}, and Slope = ",y)
+   
+if y >= 20:
+    print('you might need a larger grid')
+else:
+    print('10 x 10 grid should work...') # basically the best way i can test
+    #the function right now.  
+    
+func()
+
+print('-----------------------Come-Back-Soon--------------------------------')
+   
