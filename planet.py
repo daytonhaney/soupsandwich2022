8364368d39fad0e18f4ab0dc6059f6bbe7a91288
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-06-17 19:46:49 -0400

    Delete planet.py

--- planet.py
+++
@@ -1,7 +0,0 @@
-new_planet = ''
-planets = []
-
-while new_planet.lower() != 'done':
-    if new_planet:
-        planets.append(new_planet)
-    new_planet = input('Enter a new planet or done if done')
