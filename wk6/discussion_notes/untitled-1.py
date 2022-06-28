33fd801e978c755fb58daaa98c69fcc9d648932d
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-06-28 03:21:28 -0400

    Add files via upload

---
+++ wk6/discussion_notes/untitled-1.py
@@ -0,0 +1,16 @@
+the_data=[] 
+for updater in range (5):
+    user_prompt =int(input("Enter Data: \n")) 
+    the_data.insert(updater,user_prompt)
+    updater+=1
+    
+    
+    print("-"*70)
+def nest():
+    a = updater / user_prompt
+    print(a,updater,user_prompt)
+nest()
+        
+            
+    
+    #print(f"Thank you, and here is your data : {the_data}")
