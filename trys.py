48dd8dcaa11d508abfc98432a6432c5f9b703d4c
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-07-14 17:27:07 -0400

    Update trys.py

--- trys.py
+++ trys.py
@@ -1,9 +1,5 @@
 
 
-from curses.ascii import isalpha
-from unicodedata import name
-
-
 def user_data(name,purchase,):
     valid_name = False
     user_name = input("Please Enter your name, hit enter when done: ")
