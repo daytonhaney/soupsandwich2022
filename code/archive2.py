14f00bc03a0a5ca527b747e3c7ac52c951c69900
Jay Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-09-04 14:28:31 -0400

    measurement sendbacks

--- code/archive2.py
+++ code/archive2.py
@@ -1,6 +1,9 @@
 def size_of_house(length,width):
-    """When the workers are on-site, they take the measurments of the house
-       """
+    """When the workers are on-site, they take the measurments of the house 
+    
+    which would compute the price automatically....no thinking involved for my workers"""
+   
+      
     internal_use_only = length * width
     
         if (internal_use_only >= int(5000)):
