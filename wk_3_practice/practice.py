a0b7845893c773a7fd4a84b30b999900ef01e88b
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-02 21:29:22 -0400

    Delete practice.py

--- wk_3_practice/practice.py
+++
@@ -1,5 +0,0 @@
-cheese = ['bacon', 'gritz', 'onion']
-questions_one = f"You have a list {cheese}. You realize the name of the list is incorrect"
-
-for correct_title in cheese:
-    print(correct_title)
