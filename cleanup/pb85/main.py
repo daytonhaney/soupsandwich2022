f0cc0368a32d58267f1f57bbe6f2d0a38232e7a0
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-04 08:21:52 -0400

    Update main.py

--- cleanup/pb85/main.py
+++ cleanup/pb85/main.py
@@ -4,7 +4,7 @@ def intro():
     code_class = "cmis120\n"
     print(name,date,code_class)
 
-#def program_intro():
+# def program_intro():
 ui_spaces = " " # ui = user interface
 corp = "This program will describe the pay for paperBoy 85'\n"
 paper_boy = "Welcome To PaperBoy '85\n"
@@ -30,7 +30,7 @@ print('{:<13}{:^20}{:^20}{:>10}'.format("PRICE PER PAPER","COMMISSION","FT WEEKL
 print(ui_spaces)
 print('{:<10}{:^22}{:>15}{:>20}'.format("1.00$",".25","20.00","10.00")) 
 print(ui, '\n')
-#return program_intro
+# return program_intro
 def your_schedule():
     prompt_ForP = intput("Are you full time or part time?\n")
     if (prompt_ForP != "full time" or "Full time" or "FULL TIME" or "Full Time"): #if not full time then part time 
@@ -53,6 +53,8 @@ def your_schedule():
         big_total = full_salary + total_commission
         round_big_total = round(big_total,2)
         print(total_commission, f" is your commission, with {full_salary} your total salary \n" )
-        
-# .. tbc       
-      
+              
+def main():
+    intro()
+    your_schedule()
+main()    
