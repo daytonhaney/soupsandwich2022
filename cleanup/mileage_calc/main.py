d17d350a0bfc048f822e03c24e1c17ade5a4752d
Jason Preneveau <37848207+runtimejpp@users.noreply.github.com>
2022-10-07 10:27:29 -0400

    Update main.py

--- cleanup/mileage_calc/main.py
+++ cleanup/mileage_calc/main.py
@@ -1,24 +1,20 @@
 
-import pyfiglet # pip install pyfiglet & virtualenv. $ source bin/activate 
+import pyfiglet
  
-
-
 def main():
-    import pyfiglet # i read to keep your imports in the same venv file 
+    import pyfiglet
     
     print("-- please adjust window width if banner looks messed up")    
-    my_banner = pyfiglet.figlet_format("                   welcome") # pip3 import pyfiglet is a python3 command line utility to get colors and art in the terminal, where we run our programs. 
-    print(my_banner) #adjust width if Banner is messed up
+    my_banner = pyfiglet.figlet_format("                   welcome")
+    print(my_banner)
       
     
-    def my_introduction(name,my_class,date): #created a new greeting funcion
-        print(f"{name}\n{my_class}\n{date}") # f strings come in handy 
-    my_introduction("  Jason Preneveau", "  CMIS-120", "  19 June 2022 (updated 06 Oct 2022)") # # #since function variables are returned as strings by  defauft 
-    # You can just call three strings like naae class and date
+    def my_introduction(name,my_class,date):
+        print(f"{name}\n{my_class}\n{date}")
+    my_introduction("  Jason Preneveau", "  CMIS-120", "  19 June 2022 (updated 06 Oct 2022)") 
 main()
 
-
-_EXITLOOP = -99 #depingg on value of this variable is what allows exiting of loop   
+_EXITLOOP = -99  
 question = int(input("Number of miles driven on Monday:\n "))
 
 while question != _EXITLOOP:
@@ -41,29 +37,23 @@ while question != _EXITLOOP:
                 thursday = question
                 thursday = int(input("Number of miles driven on Friday:\n "))
                 print(f"You drove {thursday} miles on Friday.")
-                while True: # I could not ggte program to stop looping so I came up with this 
+                while True: 
                     break
                 break
             break
         break
     break
-print("Thanks for comming!") # I wanted to put the entire loop into the function but I did not have time to make the gloabl vaiables local to the function 
+
     
-
-
-
-def average(my_list): # I look the input questions and put them in a list so that i could create a bas average function to fun on it  and get the average restuls 
+def average(my_list):
     average = sum(my_list) / len(my_list)
     return average
 
 
-my_list = [question,monday,tuesday,thursday]
+my_list = [question,monday,tuesday,wednesday,thursday]
 
 avg_gas = average(my_list)
-print(f"{avg_gas} Is your average number of miles this week") # if you know your weekly average of miles you can easily guestimate how much you are speding on gas 
-
-
-#main() # 
+print(f"{avg_gas} Is your average number of miles this week.\n Come back soon.")
 
 
 
