587399f0d8a99ee2eb0c2487564071940fe49d6a
runtimejpp <jpnamausa@gmail.com>
2023-03-07 12:46:55 -0500

    func prac

---
+++ py_prac/func_prac.py/longestFunctionEver.py
@@ -0,0 +1,14 @@
+# Possible to make a script that is literrally one function?
+
+
+def longestFunction(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,t,u,v,w,x,y,z):
+    abc = input("Enter Value:\n")
+    return abc 
+
+
+
+def main():
+
+    longest = longestFunction(1,2,3,4,5,6,7,2,"one","two","three",3,4,5,2,3,10,44,22,44,5,6,"done",input(),longestFunction() )
+main()
+
