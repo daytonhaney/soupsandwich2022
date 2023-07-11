22752c0ccb4f27912c66d4f2e6a602105c733c22
jay pren <jpnamausa@gmail.com>
2023-07-10 22:28:54 -0400

    pypy

--- pydiscussion_week3/main.py
+++ pydiscussion_week3/main.py
@@ -2,26 +2,37 @@
     CMIS 120
     5 June 2022
 """
+import keyboard 
+
 print('\tWelcome to y=mx+b slope calculator\n \tEnter the values as Integers.')
 # using print statements to separate code for visibility and error handling 
 print('----------------------------------------------------------------------')
 
-x=int(input('Enter an integer for x: ')) #promping the user for 3 integers 
-b =int(input('Enter the y-intercept "b", another integer: ')) #
-m =float(input('Enter the "m", rise over run: '))
+def func(x,y):
+    x1=int(input("Enter x1:\t\t "))
+    #if x1 == syste
+    y2=int(input("Enter y1:\t\t "))
 
-y = m * x + b # Here is where i cheat...
+    x2=int(input("Enter x2:\t\t\t"))
+    y2=int(input("Enter y2:\t\t\t"))
+    dY = y2-y1 
+    dX = m(x2-x1)
 
-def func( y = m*x+b): # the original slope formula from middle school
-    return y and print(f"'You're Inputs: x={x}, m={m}, b={b}, and Slope = ",y)
-   
-if y >= 20:
-    print('you might need a larger grid')
-else:
-    print('10 x 10 grid should work...') # basically the best way i can test
+    dY/dX = m 
+
+#    m=(y-b)/x
+
+    print(m)
+    if y > 25:
+        print("====")
+    else:
+        print("-------")
+    
+    print(m)# the original slope formula from middle school
+    return m,y and print(f"'You're Inputs: x={x}, m={m}, b={b}, and Slope = ",y)
     # the function right now.  
     
-func()
+func(4,6)
 
 print('-----------------------Come-Back-Soon--------------------------------')
    
