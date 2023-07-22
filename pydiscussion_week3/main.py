0004500234abd5328f093b61a2babf81994891f4
jay pren <jpnamausa@gmail.com>
2023-07-21 20:49:24 -0400

    ../MAIN.py 	../ask = input(_whats up_).py 	../datetime 	../from time import sleep.py 	../ino.db 	../mileage_calc/1.pu 	../mileage_calc/1.py 	../mileage_calc/test.py 	../new.py 	main1.py 	../rust_vs_py/target/.rustc_info.json 	../rust_vs_py/target/debug/.cargo-lock 	../rust_vs_py/target/debug/.fingerprint/ 	../rust_vs_py/target/debug/deps/ 	../rust_vs_py/target/debug/incremental/ 	../test2.py 	../wk8/wk8_final/Learn/test.py "updated main in slope calc"

--- pydiscussion_week3/main.py
+++ pydiscussion_week3/main.py
@@ -1,7 +1,9 @@
-"""Jason Pr#$%@#$au
+"""
+Work in progress 
+Jason Pr#$%@#$au
     CMIS 120
     5 June 2022
-"""
+
 import keyboard 
 
 print('\tWelcome to y=mx+b slope calculator\n \tEnter the values as Integers.')
@@ -18,7 +20,7 @@ def func(x1,y1):
     dY = y2-y1 
     dX = m(x2-x1)
 
-    dY/dX = m 
+    m = dY/dX 
 
 #    m=(y-b)/x
 
@@ -35,4 +37,49 @@ def func(x1,y1):
 func(4,6)
 
 print('-----------------------Come-Back-Soon--------------------------------')
-   
+"""
+
+def pythonista():
+    print("+-"*34) # pythonistas will die over this 
+def m():
+    # pythonista()
+    x_vals = list()
+    y_vals = list()
+    pythonista()
+    print(
+    "This program assumes you have graphed a linear equation of a line and need to quickly find the slope (m)")
+    exit = False
+    x2 = int(input("x2: \t\t"))
+    x1 = int(input("x1: \t\t"))
+
+    pythonista() 
+    delta_x = x2 - x1
+    exit = True
+    while exit == True:
+        y2 = int(input("y2: \t\t"))
+        y1 = int(input("y1: \t\t"))
+        delta_y = y2 - y1
+        m = delta_y - delta_x
+        pythonista()
+        print(f"slope = {m}")
+        print(pythonista) 
+        x_vals.append([x2])
+        x_vals.append([x1])
+        y_vals.append([y2])
+        y_vals.append([y1])
+        print("x vals = ", x_vals)
+        print("y vals = ", y_vals)
+        return delta_x, delta_y
+    
+
+def main():
+    m()
+    while True:
+        slope = input("Press 1 to run the program again <Enter> to quit:\t\t")
+        
+        if slope == str("1"):
+            m()
+        else:
+            break
+main()
+
